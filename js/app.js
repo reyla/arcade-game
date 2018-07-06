e96738ce636fa0c173af8bb41d2c2a3127d9ed03
Kate Speed <katespeed@gmail.com>
2018-07-06 13:20:02 -0400

    Add support for WASD keys for lefties

--- js/app.js
+++ js/app.js
@@ -2,50 +2,18 @@ const player = new Player();
 const allEnemies = [...Array(3)].map((_,i)=> new Enemy(0,i+1));
 
 
-
-
-// Enemies our player must avoid
-// var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-  //  this.sprite = 'images/enemy-bug.png';
-// };
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
- //Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-// };
-
-// Draw the enemy on the screen, required method for game
-// Enemy.prototype.render = function() {
-  //  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-//};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to the
+// Player.handleInput() method. Accepts both arrow keys and WASD for lefties.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
+        65: 'left', // a
         38: 'up',
+        87: 'up', // w
         39: 'right',
-        40: 'down'
+        68: 'right', // d
+        40: 'down',
+        83: 'down' // s
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
