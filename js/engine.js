cf4c52951ebb4833858f0c800345dc0ad32b8dfa
Kate Speed <katespeed@gmail.com>
2018-07-07 15:06:06 -0400

    Move gameboard to inside html element

--- js/engine.js
+++ js/engine.js
@@ -20,13 +20,14 @@ var Engine = (function(global) {
      */
     var doc = global.document,
         win = global.window,
+        gameboard = document.getElementById('gameboard'),
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
         lastTime;
 
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+    gameboard.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
